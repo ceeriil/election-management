c7cf45bdadad9a292ebf011aa051c443cebada2e
Simon <simonorugbo15@gmail.com>
2023-06-27 06:28:25 +0100

    Calculate and display party votes and winners

--- routes/pollingAgentRoutes.js
+++ routes/pollingAgentRoutes.js
@@ -5,14 +5,16 @@ const auth = require("../middlewares/auth");
 
 router.get("/login", pollingAgentController.pollingAgent_login_get);
 router.post("/login", pollingAgentController.pollingAgent_login_post);
-
 router.get("/viewall", pollingAgentController.pollingAgent_viewAll_get);
+router.get("/view/:id", pollingAgentController.pollingAgent_view_get);
 router.get("/edit/:id", pollingAgentController.pollingAgent_edit_get);
 router.post("/edit/:id", pollingAgentController.pollingAgent_edit_post);
 router.get("/delete/:id", pollingAgentController.pollingAgent_delete_get);
 router.get("/option", pollingAgentController.pollingAgent_option_get);
 router.post("/add", pollingAgentController.pollingAgent_add_post);
 router.get("/add", pollingAgentController.pollingAgent_add_get);
-router.get("/reportmalpractice", pollingAgentController.pollingAgent_report_get);
-
+router.get(
+  "/reportmalpractice",
+  pollingAgentController.pollingAgent_report_get
+);
 module.exports = router;
--- routes/pollingUnitRoutes.js
+++ routes/pollingUnitRoutes.js
@@ -4,5 +4,6 @@ const pollingUnitController = require("../controllers/pollingUnitController");
 
 router.get("/login", pollingUnitController.pollingUnit_login_get);
 router.post("/login", pollingUnitController.pollingUnit_login_post);
+router.get("/results", pollingUnitController.pollingUnit_results_get); // Add this line
 
 module.exports = router;
